25b7df6cf68a5a3476aa9f708d88a85587bea253
Macarena Rivera <89401341+MacarenaRivera@users.noreply.github.com>
2021-10-06 10:51:07 -0300

    Maqueta html y css + info pokemon

--- src/main.js
+++ src/main.js
@@ -1,6 +1,27 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+//esto se debe usar cuando creemos funciones de filtrar
+//import { example } from './data.js';
 
-console.log(example, data);
+
+const pokemon= data.pokemon;
+console.log ("pokemon",pokemon);
+
+//variable que contiene el div en html
+const callPokeContainer = document.getElementById("pokemonContainer");
+
+const listPokemon = (pokemon) => {
+  return `
+   <div class="pokemonBox">
+         <div class="box" >
+         <p class= "numberBox">N° ${pokemon.num}</p>
+         <p> <img  class="picture" src=" ${pokemon.img}"> </p>
+         </div>
+         <p class="card-title"> ${pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)} </p>
+   </div>      
+      `;
+    };
+
+  for (let i=0; i < pokemon.length; i++){
+    callPokeContainer.innerHTML += listPokemon(pokemon[i]);
+
+}   
