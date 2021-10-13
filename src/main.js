0c4a6b835659327d1c7116871cdf42fe24d06538
alemaureira22 <89367685+alemaureira22@users.noreply.github.com>
2021-10-13 19:01:54 -0300

    Rama ale (#2)
    * prueba
    
    * barradelfilto
    
    * agregarbody
    
    * filter y sort ok

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import data from './data/pokemon/pokemon.js';
 //esto se debe usar cuando creemos funciones de filtrar
-//import { example } from './data.js';
-
+import {filterData} from './data.js';
+import { pokemonOrder } from './data.js';
 
 const pokemon= data.pokemon;
 console.log ("pokemon",pokemon);
+
 
 //variable que contiene el div en html
 const callPokeContainer = document.getElementById("pokemonContainer");
@@ -24,4 +25,29 @@ const listPokemon = (pokemon) => {
   for (let i=0; i < pokemon.length; i++){
     callPokeContainer.innerHTML += listPokemon(pokemon[i]);
 
-}   
+} 
+
+let opcionElement=document.getElementById("typePokemon");
+opcionElement.addEventListener("change", (event)=> {
+  const choseElement=filterData(pokemon, event.target.value);
+  const print=(type)=>{
+    callPokeContainer.innerHTML="";
+    for(let i=0; i < type.length;i++){
+      callPokeContainer.innerHTML +=listPokemon(type[i]);
+    }
+  }
+  print(choseElement);
+});
+
+let selectorByOrder=document.getElementById("selectOrder");
+selectorByOrder.addEventListener("change",()=>{
+const callPokeOrder=pokemonOrder(pokemon,selectorByOrder.value);
+console.log(selectorByOrder.value)
+const print=(upward)=>{
+  callPokeContainer.innerHTML=""
+for(let i=0; i < upward.length;i++){
+  callPokeContainer.innerHTML +=listPokemon(upward[i]);
+}
+}
+ print (callPokeOrder);
+});
