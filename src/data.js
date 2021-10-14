be911f1f5af78d7949cab748a5dc972a5deca336
alemaureira22 <jandrita_18@hotmail.com>
2021-10-14 18:56:58 -0300

    filter y sort ok

--- src/data.js
+++ src/data.js
@@ -1,10 +1,33 @@
 
 // estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
-};
+//export const example = () => {
+ // return 'example';
+//};
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+//export const anotherExample = () => {
+//  return 'OMG';
+//};
+
+ export const filterData=(data,searchElement) =>{
+  
+  const resultFilter= data.filter((pokemon) => pokemon.type.includes(searchElement));
+  return resultFilter;
+ };
+
+  export const pokemonOrder=(pokemones,order)=>{
+    const datapoke=pokemones;
+    const orderByInicial= datapoke.sort((prev,next)=>{
+    if (prev.name > next.name){
+      return 1;
+    }if (prev.name < next.name){
+      return -1;
+    }
+    return 0;
+   });
+   console.log(order);
+   if(order==='descendente'){
+     return orderByInicial.reverse();
+   }
+   return orderByInicial;
+ };
