6f5e5d9f184e84efdbed1cdbee66708ce9e623c7
Macarena Rivera <89401341+MacarenaRivera@users.noreply.github.com>
2021-10-14 22:27:22 -0300

    reemplazofiltrosok

--- src/data.js
+++ src/data.js
@@ -1,33 +1,38 @@
 
-// estas funciones son de ejemplo
-
-//export const example = () => {
- // return 'example';
-//};
-
-//export const anotherExample = () => {
-//  return 'OMG';
-//};
-
- export const filterData=(data,searchElement) =>{
-  
-  const resultFilter= data.filter((pokemon) => pokemon.type.includes(searchElement));
+//funcion filtar por tipo
+export const filterData = (data, searchElement) => {
+  if (searchElement === "all") {
+    return data;
+  }
+  const resultFilter = data.filter((pokemon) =>
+    pokemon.type.includes(searchElement)
+  );
   return resultFilter;
- };
+};
 
-  export const pokemonOrder=(pokemones,order)=>{
-    const datapoke=pokemones;
-    const orderByInicial= datapoke.sort((prev,next)=>{
-    if (prev.name > next.name){
+//funcion ordenar de la a-z y z-a
+export const pokemonOrder = (pokemons, order) => {
+  const datapoke = pokemons;
+  const orderByInicial = datapoke.sort((prev, next) => {
+    if (prev.name > next.name) {
       return 1;
-    }if (prev.name < next.name){
+    }
+    if (prev.name < next.name) {
       return -1;
     }
     return 0;
-   });
-   console.log(order);
-   if(order==='descendente'){
-     return orderByInicial.reverse();
-   }
-   return orderByInicial;
- };
+  });
+
+  if (order === "descendente") {
+    return orderByInicial.reverse();
+  }
+  return orderByInicial;
+};
+
+//funcion para buscar por nombre o numero
+export const searchId = (pokemons, search) => {
+  return pokemons.filter(
+    (dataPokemon) =>
+      dataPokemon.name.includes(search) || dataPokemon.num.includes(search)
+  );
+};
--- src/main.js
+++ src/main.js
@@ -1,53 +1,57 @@
-import data from './data/pokemon/pokemon.js';
-//esto se debe usar cuando creemos funciones de filtrar
-import {filterData} from './data.js';
-import { pokemonOrder } from './data.js';
+import data from "./data/pokemon/pokemon.js";
 
-const pokemon= data.pokemon;
-console.log ("pokemon",pokemon);
+import { filterData } from "./data.js";
+import { pokemonOrder } from "./data.js";
+import { searchId } from "./data.js";
 
+const dataPokemon = Object.values(data.pokemon);
 
 //variable que contiene el div en html
 const callPokeContainer = document.getElementById("pokemonContainer");
 
-const listPokemon = (pokemon) => {
-  return `
-   <div class="pokemonBox">
+//Impresion de Pantilla para mostrar todos los pokemons
+const pokeList = (data) => {
+  let listPoke = "";
+  for (let i = 0; i < data.length; i++) {
+    listPoke += ` 
+    <div class="pokemonBox">
          <div class="box" >
-         <p class= "numberBox">N° ${pokemon.num}</p>
-         <p> <img  class="picture" src=" ${pokemon.img}"> </p>
-         </div>
-         <p class="card-title"> ${pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)} </p>
-   </div>      
-      `;
-    };
-
-  for (let i=0; i < pokemon.length; i++){
-    callPokeContainer.innerHTML += listPokemon(pokemon[i]);
-
-} 
-
-let opcionElement=document.getElementById("typePokemon");
-opcionElement.addEventListener("change", (event)=> {
-  const choseElement=filterData(pokemon, event.target.value);
-  const print=(type)=>{
-    callPokeContainer.innerHTML="";
-    for(let i=0; i < type.length;i++){
-      callPokeContainer.innerHTML +=listPokemon(type[i]);
-    }
+         <p class= "numberBox">N° ${data[i].num}</p>
+         <p> <img  class="picture" src=" ${data[i].img}"> </p>
+        </div>
+        <p class="card-title"> ${data[i].name.charAt(0).toUpperCase() + data[i].name.slice(1)} </p>
+    </div>      
+     `;
   }
-  print(choseElement);
+  callPokeContainer.innerHTML = listPoke;
+};
+
+pokeList(dataPokemon);
+
+//Funcion de filter
+let opcionElement = document.getElementById("typePokemon");
+opcionElement.addEventListener("change", (event) => {
+  callPokeContainer.innerHTML = "";
+  const choseElement = filterData(dataPokemon, event.target.value);
+
+  pokeList(choseElement);
 });
 
-let selectorByOrder=document.getElementById("selectOrder");
-selectorByOrder.addEventListener("change",()=>{
-const callPokeOrder=pokemonOrder(pokemon,selectorByOrder.value);
-console.log(selectorByOrder.value)
-const print=(upward)=>{
-  callPokeContainer.innerHTML=""
-for(let i=0; i < upward.length;i++){
-  callPokeContainer.innerHTML +=listPokemon(upward[i]);
-}
-}
- print (callPokeOrder);
+//Funcion sort
+let selectorByOrder = document.getElementById("selectOrder");
+selectorByOrder.addEventListener("change", () => {
+  callPokeContainer.innerHTML = "";
+  const callPokeOrder = pokemonOrder(dataPokemon, selectorByOrder.value);
+
+  pokeList(callPokeOrder);
+});
+
+//Funcion buscar por id
+let searchIdOrNumber = document.getElementById("inputPokemon");
+searchIdOrNumber.addEventListener("keyup", () => {
+  let convertingToMin = searchIdOrNumber.value.toLowerCase();
+  callPokeContainer.innerHTML = "";
+  const inputIdPokemon = searchId(dataPokemon, convertingToMin);
+
+  pokeList(inputIdPokemon);
 });
