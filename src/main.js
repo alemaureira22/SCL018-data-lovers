94bb0579eeff5814ec22a899a250ca6dffda2b57
Macarena Rivera <89401341+MacarenaRivera@users.noreply.github.com>
2021-10-21 10:33:22 -0300

    despliegue modal

--- src/main.js
+++ src/main.js
@@ -6,9 +6,9 @@ import { searchId } from "./data.js";
 
 const dataPokemon = Object.values(data.pokemon);
 
-
 //variable que contiene el div en html
 const callPokeContainer = document.getElementById("pokemonContainer");
+
 
 //Impresion de Pantilla para mostrar todos los pokemons
 const pokeList = (data) => {
@@ -16,18 +16,89 @@ const pokeList = (data) => {
   for (let i = 0; i < data.length; i++) {
     listPoke += ` 
     <div class="pokemonBox">
-         <div class="box" >
+         <button id="${data[i].name}" class="box"  >
          <p class= "numberBox">N° ${data[i].num}</p>
          <p> <img  class="picture" src=" ${data[i].img}"> </p>
-        </div>
-        <p class="card-title"> ${data[i].name.charAt(0).toUpperCase() + data[i].name.slice(1)} </p>
-    </div>      
+        </button>
+        <p class="card-title"> ${
+          data[i].name.charAt(0).toUpperCase() + data[i].name.slice(1)
+        } </p>
+    </div>  
+    
+    
+    <div class="containerModal"  id="${data[i].name}">
+      <div class="modalInside">
+       <button class="close" id="x"> <img src="data/imagenes/icons8xbox.png" /></button>
+        <p class= "numberModal">N°${data[i].num}</p>
+        <div class="imgContainer"><img  class="pictureModal" src=" ${
+          data[i].img
+        }"> </div>
+        <div class="textPicture">
+        <h2 class="nameModal"> ${
+          data[i].name.charAt(0).toUpperCase() + data[i].name.slice(1)
+        } <img class="miniPokeball" src="data/imagenes/icons8pokeball.png" /> </h2>
+        <table class="sizeTy">
+           <tr>
+           <hr/>
+            <th>Types</th> 
+            <th>Heigth</th>
+            <th>Weigth</th>
+           </tr>
+           <tr>
+            <td>${data[i].type.join(" <br> ")}</td> 
+            <td>${data[i].size.height}</td>
+            <td>${data[i].size.weight}</td>
+           </tr>
+          </table>
+         <div>
+         <hr>
+          <p class="titles">Description:</p>
+          <p class="aboutModal"> ${data[i].about} </p>
+          <p class="resistantModal"> <strong>Resistant:</strong> ${data[
+            i
+          ].resistant.join(" - ")} </p>
+          <p class="weaknessesModal"> <strong>Weaknesses:</strong> ${data[
+            i
+          ].weaknesses.join(" - ")} </p>
+          </div>
+          <table class="tableModal">
+           <tr>
+            <th class="stables">Base-attack</th> 
+            <th class="stables">Base-defense</th>
+            <th class="stables">Base-stamina</th>
+           </tr>
+           <tr>
+            <td class="typesS">${data[i].stats["base-attack"]}</td> 
+            <td class="typesS">${data[i].stats["base-defense"]}</td>
+            <td class="typesS">${data[i].stats["base-stamina"]}</td>
+           </tr>
+          </table>
+        
+       </div>
+
+      </div>
+    </div>
      `;
   }
   callPokeContainer.innerHTML = listPoke;
 };
+//Funcion que despliega el modal
+function addEvents() {
+  const btnBoxes = document.querySelectorAll(".box");
+  btnBoxes.forEach((element)=>{
+    const currentId = element.getAttribute("id")
+    element.addEventListener("click", ()=>{
+      const modal = document.querySelector(`.containerModal#${currentId}`)
+      modal.style.display="block"
+    })
+
+  })
+}
+
 
 pokeList(dataPokemon);
+addEvents();
+
 
 //Funcion de filter
 let opcionElement = document.getElementById("typePokemon");
@@ -36,6 +107,7 @@ opcionElement.addEventListener("change", (event) => {
   const choseElement = filterData(dataPokemon, event.target.value);
 
   pokeList(choseElement);
+  addEvents();
 });
 
 //Funcion sort
@@ -45,6 +117,7 @@ selectorByOrder.addEventListener("change", () => {
   const callPokeOrder = pokemonOrder(dataPokemon, selectorByOrder.value);
 
   pokeList(callPokeOrder);
+  addEvents();
 });
 
 //Funcion buscar por id
@@ -55,4 +128,5 @@ searchIdOrNumber.addEventListener("keyup", () => {
   const inputIdPokemon = searchId(dataPokemon, convertingToMin);
 
   pokeList(inputIdPokemon);
+  addEvents();
 });
