fdfc7ff4dd57b16a37b8da7c48a1bcd20373f897
alemaureira22 <89367685+alemaureira22@users.noreply.github.com>
2021-10-25 20:50:07 -0200

    Rama ale (#2)
    * Maqueta html y css + info pokemon
    
    * bienvenida ok
    
    * Rama ale (#1)
    
    * prueba
    
    * barradelfilto
    
    * prueba
    
    * barradelfilto
    
    * responsiveok
    
    * agregarbody
    
    * Rama ale (#2)
    
    * prueba
    
    * barradelfilto
    
    * agregarbody
    
    * filter y sort ok
    
    * filter y sort ok
    
    * reemplazofiltrosok
    
    * css filtros ok
    
    * Rama ale (#3)
    
    * prueba
    
    * barradelfilto
    
    * agregarbody
    
    * filter y sort ok
    
    * despliegue modal
    
    * carta ok
    
    * readme
    
    Co-authored-by: Macarena Rivera <89401341+MacarenaRivera@users.noreply.github.com>

--- src/data.js
+++ src/data.js
@@ -1,9 +1,37 @@
-// estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
+//funcion filtar por tipo
+export const filterData = (data, searchElement) => {
+  if (searchElement === "all") {
+    return data;
+  }
+  const resultFilter = data.filter((pokemon) =>
+    pokemon.type.includes(searchElement)
+  );
+  return resultFilter;
 };
 
-export const anotherExample = () => {
-  return 'OMG';
+//funcion ordenar de la a-z y z-a
+export const pokemonOrder = (pokemons, order) => {
+  const orderByInicial =pokemons.sort((a,b) => {
+    if (a.name > b.name) {
+      return 1;
+    }
+    if (a.name < b.name) {
+      return -1;
+    }
+    
+  });
+
+  if (order === "falling") {
+    return orderByInicial.reverse();
+  }
+  return orderByInicial;
+};
+
+//funcion para buscar por nombre o numero
+export const searchId = (pokemons, search) => {
+  return pokemons.filter(
+    (dataPokemon) =>
+      dataPokemon.name.includes(search) || dataPokemon.num.includes(search)
+  );
 };
--- src/main.js
+++ src/main.js
@@ -1,6 +1,136 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import data from "./data/pokemon/pokemon.js";
 
-console.log(example, data);
+import { filterData } from "./data.js";
+import { pokemonOrder } from "./data.js";
+import { searchId } from "./data.js";
+
+const dataPokemon = Object.values(data.pokemon);
+
+
+//variable que contiene el div en html
+const callPokeContainer = document.getElementById("pokemonContainer");
+
+//Impresion de Pantilla para mostrar todos los pokemons
+const pokeList = (data) => {
+  let listPoke = "";
+  for (let i = 0; i < data.length; i++) {
+    listPoke += ` 
+    <div class="pokemonBox">
+         <button id="${data[i].num}" class="box"  >
+         <p class= "numberBox">N° ${data[i].num}</p>
+         <p> <img  class="picture" src=" ${data[i].img}"> </p>
+        </button>
+        <p class="card-title"> ${
+          data[i].name.charAt(0).toUpperCase() + data[i].name.slice(1)
+        } </p>
+    </div>  
+    
+    
+    <div class="containerModal"  id="k-${data[i].num}">
+      <div class="modalInside">
+      <span class="close" id="close">&times;</span>
+        <p class= "numberModal">N°${data[i].num}</p>
+        <div class="imgContainer"><img  class="pictureModal" src=" ${
+          data[i].img
+        }"> </div>
+        <div class="textPicture">
+          <h2 class="nameModal"> ${
+            data[i].name.charAt(0).toUpperCase() + data[i].name.slice(1)
+          } <img class="miniPokeball" src="data/imagenes/icons8pokeball.png" /> </h2>
+          <table class="sizeTy">
+           <tr>
+           <hr/>
+            <th>Types</th> 
+            <th>Heigth</th>
+            <th>Weigth</th>
+           </tr>
+           <tr>
+            <td>${data[i].type.join(" <br> ")}</td> 
+            <td>${data[i].size.height}</td>
+            <td>${data[i].size.weight}</td>
+           </tr>
+          </table>
+         <div>
+         <hr>
+          <p class="titles">Description:</p>
+          <p class="aboutModal"> ${data[i].about} </p>
+          <p class="resistantModal"> <strong>Resistant:</strong> ${data[
+            i
+          ].resistant.join(" - ")} </p>
+          <p class="weaknessesModal"> <strong>Weaknesses:</strong> ${data[
+            i
+          ].weaknesses.join(" - ")} </p>
+          </div>
+          <table class="tableModal">
+           <tr>
+            <th class="stables">Base-attack</th> 
+            <th class="stables">Base-defense</th>
+            <th class="stables">Base-stamina</th>
+           </tr>
+           <tr>
+            <td class="typesS">${data[i].stats["base-attack"]}</td> 
+            <td class="typesS">${data[i].stats["base-defense"]}</td>
+            <td class="typesS">${data[i].stats["base-stamina"]}</td>
+           </tr>
+          </table>
+        
+      
+       </div>
+
+      </div>
+    </div>
+     `;
+  }
+  callPokeContainer.innerHTML = listPoke;
+};
+//Funcion que despliega el modal
+function addEvents() {
+  const btnBoxes = document.querySelectorAll(".box");
+  btnBoxes.forEach((element) => {
+    const currentId = "k-" + element.getAttribute("id");
+    element.addEventListener("click", () => {
+      const modal = document.querySelector(`.containerModal#${currentId}`);
+      modal.style.display = "block";
+      let buttonCloser = document.querySelectorAll(".close");
+      buttonCloser.forEach((element) => {
+        element.addEventListener("click", () => {
+          modal.style.display = "none";
+        });
+      });
+    });
+  });
+}
+
+pokeList(dataPokemon);
+addEvents();
+
+//Funcion de filter
+let opcionElement = document.getElementById("typePokemon");
+opcionElement.addEventListener("change", (event) => {
+  callPokeContainer.innerHTML = "";
+  const choseElement = filterData(dataPokemon, event.target.value);
+
+  pokeList(choseElement);
+  addEvents();
+});
+
+//Funcion sort
+let selectorByOrder = document.getElementById("selectOrder");
+selectorByOrder.addEventListener("change", () => {
+  callPokeContainer.innerHTML = "";
+  const callPokeOrder = pokemonOrder(dataPokemon, selectorByOrder.value);
+
+  pokeList(callPokeOrder);
+  addEvents();
+});
+
+//Funcion buscar por id
+let searchIdOrNumber = document.getElementById("inputPokemon");
+searchIdOrNumber.addEventListener("keyup", () => {
+  let convertingToMin = searchIdOrNumber.value.toLowerCase();
+  callPokeContainer.innerHTML = "";
+  const inputIdPokemon = searchId(dataPokemon, convertingToMin);
+
+  pokeList(inputIdPokemon);
+  addEvents();
+});
